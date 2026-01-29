fe0a93b67242c0bd9e53dd3958faf68aef93d643
neuwcodebox <neuwgmbox@gmail.com>
2026-01-29 23:20:45 +0900

    feat: update event level palette

--- src/components/disaster-map/DisasterMapPalette.ts
+++ src/components/disaster-map/DisasterMapPalette.ts
@@ -3,11 +3,11 @@ import { EventLevels } from '../../types';
 const REGION_FILL_ALPHA = 70;
 
 export const LEVEL_COLORS: Record<EventLevels, [number, number, number, number]> = {
-  [EventLevels.Info]: [160, 170, 180, 180],
-  [EventLevels.Minor]: [70, 130, 230, 200],
-  [EventLevels.Moderate]: [232, 210, 128, 210],
-  [EventLevels.Severe]: [248, 140, 70, 220],
-  [EventLevels.Critical]: [244, 112, 120, 230],
+  [EventLevels.Info]: [98, 116, 142, 180],
+  [EventLevels.Minor]: [21, 93, 252, 200],
+  [EventLevels.Moderate]: [240, 177, 0, 210],
+  [EventLevels.Severe]: [245, 74, 0, 220],
+  [EventLevels.Critical]: [193, 0, 7, 230],
 };
 
 export const LEVEL_RADII: Record<EventLevels, number> = {
